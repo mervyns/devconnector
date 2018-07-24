c5b3fc232f16dd9d21cdd9bc3bb71eda03c997fd
Mervyn Tan <mervyns@gmail.com>
2018-07-24 23:09:18 +0800

    trying to fix a bug

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
 
 const router = express.Router();
 
+// Load Profile model
+const Profile = require('../../models/Profile');
+// Load User models
+const User = require('../../models/User');
+
+// @route GET api/profile/Test
+// @desc Tests profile routes
+// @access Public
 router.get('/test', (req, res) => res.json({ msg: 'Profile Works' }));
+
+// @route GET api/profile/Test
+// @desc Tests profile routes
+// @access Private
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ user: req.user.id })
+    .then((profile) => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        return res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
 
 module.exports = router;
