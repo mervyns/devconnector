ca3247e033eb445131c16477e8c2f2eada9df0e0
Mervyn Tan <mervyns@gmail.com>
2018-08-01 01:46:37 +0800

    Created PostForm component with redux state

--- client/src/App.js
+++ client/src/App.js
@@ -19,8 +19,9 @@ import EditProfile from "./components/edit-profile/EditProfile";
 import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
-import Profile from './components/profile/Profile';
-import NotFound from './components/not-found/NotFound';
+import Profile from "./components/profile/Profile";
+import NotFound from "./components/not-found/NotFound";
+import Posts from "./components/posts/Posts";
 
 import "./App.css";
 
@@ -88,6 +89,9 @@ class App extends Component {
                 path="/add-education"
                 component={AddEducation}
               />
+            </Switch>
+            <Switch>
+              <PrivateRoute exact path="/feed" component={Posts} />
             </Switch>
             <Route exact path="/not-found" component={NotFound} />
             <Footer />
