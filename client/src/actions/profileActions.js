b5d9f7122b9589d836e1402dc78d117424873233
Mervyn Tan <mervyns@gmail.com>
2018-07-31 11:58:24 +0800

    added delete account functionality on front end

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -4,7 +4,8 @@ import {
   GET_PROFILE,
   PROFILE_LOADING,
   GET_ERRORS,
-  CLEAR_CURRENT_PROFILE
+  CLEAR_CURRENT_PROFILE,
+  SET_CURRENT_USER
 } from "./types";
 
 // Get Current Profile
@@ -26,6 +27,19 @@ export const getCurrentProfile = () => dispatch => {
     );
 };
 
+// Create Profile
+export const createProfile = (profileData, history) => dispatch => {
+  axios
+    .post("/api/profile", profileData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 // Profile Loading
 export const setProfileLoading = () => {
   return {
@@ -38,4 +52,21 @@ export const clearCurrentProfile = () => {
   return {
     type: CLEAR_CURRENT_PROFILE
   };
+};
+
+// Delete Account and Profile
+export const deleteAccount = () => dispatch => {
+  if (window.confirm("Are you sure? This cannot be undone")) {
+    axios.delete("/api/profiile").then(res =>
+      dispatch({
+        type: SET_CURRENT_USER,
+        payload: {}
+      }).catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      )
+    );
+  }
 };
