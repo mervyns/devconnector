99c44b5473cb635995f4f5d8e077a7f180ddb02b
Mervyn Tan <mervyns@gmail.com>
2018-08-01 17:52:03 +0800

    Created Single Post Display Component

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -4,7 +4,8 @@ import {
   GET_ERRORS,
   GET_POSTS,
   POST_LOADING,
-  DELETE_POST
+  DELETE_POST,
+  GET_POST
 } from "./types";
 
 // Add Post
@@ -25,7 +26,7 @@ export const addPost = postData => dispatch => {
     );
 };
 
-// Get Post
+// Get Posts
 export const getPosts = () => dispatch => {
   dispatch(setPostLoading());
   axios
@@ -39,6 +40,25 @@ export const getPosts = () => dispatch => {
     .catch(err =>
       dispatch({
         type: GET_POSTS,
+        payload: null
+      })
+    );
+};
+
+// Get Post
+export const getPost = id => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .get(`/api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POST,
         payload: null
       })
     );
